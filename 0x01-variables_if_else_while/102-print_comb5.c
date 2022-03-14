6321d8f8e3650a53b41fb69acc3980ad717914ed
marybanire <baniremary@gmail.com>
2022-03-13 17:17:50 -0700

    combinations of two two-digit number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,6 +18,7 @@ if (i < j)
 putchar((i / 10) + 48);
 putchar((i % 10) + 48);
 putchar(' ');
+putchar((j / 10) + 48);
 putchar((j % 10) + 48);
 if (i != 98 || j != 99)
 {
@@ -29,4 +30,4 @@ putchar(' ');
 }
 putchar('\n');
 return (0);
-}	
+}
