062d1f4de9827edb59fc84cf59d54dcb9e69401c
marybanire <baniremary@gmail.com>
2022-03-13 18:02:14 -0700

    numbers stored in variable(n) is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
 /**
-* main - entry block
-* @void: no argument
-* Return: 0 (Success)
-*/
+ * main - entry block
+ * @void: no argumet
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+srand(time((0);
+n = rand() -RAND_MAX / 2;
 if (n > 0)
-{
 printf("%d is positive\n", n);
-}
 else if (n == 0)
-{
 printf("%d is zero\n", n);
-}
-else
-{
+else if (n < 0)
 printf("%d is negative\n", n);
-}
 return (0);
 }
