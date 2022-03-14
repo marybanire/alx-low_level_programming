c9ceeee9070b1f472d24c7fa2a43ed402fd4daea
marybanire <baniremary@gmail.com>
2022-03-14 01:37:12 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints exactly the phrase "Programming is like building a multilingual puzzle
+ * main - prints exactly "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
