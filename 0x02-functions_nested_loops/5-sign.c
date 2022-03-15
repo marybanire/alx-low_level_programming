f5721819f61a0664ed8ac3caaaf9ddd4bd1bf42e
marybanire <baniremary@gmail.com>
2022-03-15 06:02:36 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,6 +7,7 @@
  * @n: number to check for sign
  *
  * Return: 1 if positive, 0 if 0, and -1 if negative
+ *
  **/
 
 int print_sign(int n)
@@ -16,7 +17,7 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else if (n ==0)
+else if (n == 0)
 {
 _putchar('0');
 return (0);
