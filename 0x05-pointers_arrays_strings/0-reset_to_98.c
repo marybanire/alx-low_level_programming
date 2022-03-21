57636fe8b97ec79579726e578bbecead5c629fb3
marybanire <baniremary@gmail.com>
2022-03-21 01:17:09 -0700

    set value of int to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - value of int to 98
+ * @n: address of value to change
+ * Description: reset *n to 98
+ * Return: nothing
+ **/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
