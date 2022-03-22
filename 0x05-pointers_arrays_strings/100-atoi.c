f7ff0bd7174d2e4112f3358524db2869936a74ed
marybanire <baniremary@gmail.com>
2022-03-22 23:25:36 +0100

    converts string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,12 +10,12 @@ int _atoi(char *s)
 int i, val, sign;
 val = 0;
 sign = 1;
-for (i =0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
+for (i = 0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
 {
 if (s[i] == '-')
 sign = sign * -1;
 }
-for ( i = 0; s[i] != 0; i++)
+for (i = 0; s[i] != 0; i++)
 {
 if (s[i] >= '0' && s[i] <= '9')
 val = val * 10 + sign * (s[i] - '0');
