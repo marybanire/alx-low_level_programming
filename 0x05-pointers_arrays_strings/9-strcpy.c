bcd2d28d704d2868eed663f5f34e588804563626
marybanire <baniremary@gmail.com>
2022-03-22 23:01:52 +0100

    copies string src to buffer pointed to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string src to buffer pointed to by dest
+ * @dest: sting pointer
+ * @src: buffer pointer
+ * Description: copies string src to buffer
+ * Return: dest pointer
+ **/
+char *_strcpy(char *dest, char *src)
+int i;
+i = 0;
+while (i >= 0)
+{
+dest[i] = src[i];
+if (src[i] == '\0')
+return (dest);
+i++;
+}
+return (dest);
+}
