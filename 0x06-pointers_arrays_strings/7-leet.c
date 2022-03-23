1ab2718a4fec61416ba5e20adecf99f55d4961b1
marybanire <baniremary@gmail.com>
2022-03-23 22:12:53 +0100

    convert string to leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,13 +10,13 @@ char *leet(char *c)
 {
 int i, j;
 char *replace;
-replace = "aAeEoOtTL4433007711";
+replace = "aAeEoOtTlL4433007711";
 for (i = 0; c[i] != '\0'; i++)
 {
 for (j = 0; j <= 9; j++)
 {
 if (c[i] == replace[j])
-c[i] = replace[j +10];
+c[i] = replace[j + 10];
 }
 }
 return (c);
