df4f955924c9c1e76d190eeb6d9fc584a563327b
marybanire <baniremary@gmail.com>
2022-03-24 18:16:52 +0100

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
+
 i = 0;
+
 while (dest[i] != '\0')
 i++;
+
 for (j = 0; j < n; j++)
 {
 dest[i] = src[j];
@@ -20,5 +23,6 @@ i++;
 }
 if (dest[i - 1] != '\0')
 dest[i] = '\0';
+
 return (dest);
 }
