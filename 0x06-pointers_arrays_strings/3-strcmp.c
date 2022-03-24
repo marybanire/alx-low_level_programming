369e0f892750dc78f85a3abfae7b654a74a5be76
marybanire <baniremary@gmail.com>
2022-03-24 21:00:09 +0100

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,8 +2,7 @@
 /**
  * _strcmp - compares two strings
  * @s1: first string.
- * @s2: second string. 
- * Description: compares two strings
+ * @s2: second string.
  * Return: 0 if s1 and s2 are equals,
  * another number if not.
  */
