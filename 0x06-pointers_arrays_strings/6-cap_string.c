35e013117d37fb5c61bb7ebe438cb891be230496
marybanire <baniremary@gmail.com>
2022-03-24 21:40:29 +0100

    capitalizes all word of a sting

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,55 +1,31 @@
 #include "main.h"
 #include <stddef.h>
-#include <stdio.h>
+#include <stdio.h>}
 /**
+ * cap_string - capitalizes all words a string
+ * @s: input string 
+ * Return: s
+ **/
 
- * upper - capitalizes a letter if it is lowercase
- * @c: pointer to a string
- * Return: pointer to string
- **/
-char *upper(char *c)
+char *cap_string(char *s)
 {
-if (c[0] >= 'a' && c[0] <= 'z')
-c[0] -= 32;
-return (c);
-}
-
-/**
- * new_word - compares char to chars that inicates a new word
- * @c: char to check
- * Return: 1 if char is a word separator, 0 of not
- **/
-int new_word(char c)
+int count = 0, i;
+int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
 {
-int i = 0;
-char *word = " \t\n,;.!?\"(){}";
-while (word[i] != '\0')
+for (i = 0; i < 13; i++)
+{{
+if (*(s + count) == sep_words[i])
 {
-if (c == word[i])
-return (1);
-i++;
-}
-return (0);
-}
-/**
- * cap_string - capitalize all words in a string
- * @str: string to modify
- * Return: pointer to str
- **/
-char *cap_string(char *str)
-{
-int i;
-if (str == NULL)
-return (NULL);
-upper(str);
-if (str[0] == '\0')
-return (str);
-for (i = 1; str[i] != '\0'; i++)
-{
-if (new_world(str[i -1]) == 1)
-{
-upper(&str[i]);
+if ((*(s + (count +1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
 }
 }
-return (str);
+count++;
+}
+return (s);
 }
