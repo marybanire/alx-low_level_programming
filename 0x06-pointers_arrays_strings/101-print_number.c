eb02d5b4cdf5989fc1ea6ea282c35e7bbf4743ca
marybanire <baniremary@gmail.com>
2022-03-24 17:47:24 +0100

    prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_number - prints an integer
- * @n: input integer
- * Return: no return
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 
 void print_number(int n)
