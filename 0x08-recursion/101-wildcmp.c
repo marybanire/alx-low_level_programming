bf5e887f941bf081048c5225fbb691448c318946
marybanire <baniremary@gmail.com>
2022-03-31 00:41:49 +0100

    compares strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,9 +13,9 @@ if (*s1 == '\0')
 {
 if (*s2 != '\0' && *s2 == '*')
 {
-return (wildcmp(s1, s2 + 1 ));
+return (wildcmp(s1, s2 + 1));
 }
-return (*s2 =='\0');
+return (*s2 == '\0');
 }
 if (*s2 == '*')
 {
