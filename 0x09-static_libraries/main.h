ba4fc8e853ed1d6d19b8be2f6a8186f7998e1d4c
marybanire <baniremary@gmail.com>
2022-04-03 15:40:50 +0100

    libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -8,7 +9,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void puts(char *s);
+void puts(char*s);
 char *_srtcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
