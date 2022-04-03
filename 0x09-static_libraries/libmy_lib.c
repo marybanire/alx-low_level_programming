65a5cde370b5948858c5e75bbe1756e6de41da9a
marybanire <baniremary@gmail.com>
2022-04-03 16:36:54 +0100

    libraries

--- 0x09-static_libraries/libmy_lib.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-#include <unistd.h>
-#include <stdio.h>
-/* Filename: libmy_lib.c */
-int main(void)
-{
-_puts("At the end of the day, my goal was to be the best hacker")
-return (0);
