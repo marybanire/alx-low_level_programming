fcb24c358c2d1a1326e110f73c11332b70b8b32a
marybanire <baniremary@gmail.com>
2022-04-07 14:27:44 +0100

    allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
-  malloc_checked - allocates memory using malloc
- * @b: unsigned integer
- * Return: returns a pointer to the allocated memory, f malloc fails,
- * the malloc_checked function should cause normal process termination
- * with a status value of 98
+ * malloc_checked - allocates memory using malloc
+ * @b: bytes allocate
+ * Return: pointer
  */
-
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
 
 ptr = malloc(b);
-
 if (ptr == NULL)
 exit(98);
 return (ptr);
