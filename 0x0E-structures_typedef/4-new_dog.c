f63dcaac5c19e1cb2fdd6bc66050cf64fc8338f8
marybanire <baniremary@gmail.com>
2022-04-11 13:14:56 +0100

    Writes a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,76 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * new_dog - creates dog object
- * @name: dog name
- * @age: age of dog
- * @owner: owner of dog
- * Return: dog object, NULL if fails
- **/
+ *new_dog - creates a new dog.
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *Return: new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+char *nname, *nowner;
+dog_t *ndog;
 
-
-if (hobbes == NULL)
+if (name == NULL || owner == NULL)
 return (NULL);
 
+ndog = malloc(sizeof(dog_t));
+if (ndog == NULL)
+return (NULL);
 
-return (hobbes);
+nname = malloc(sizeof(char) * _strl(name) + 1);
+if (nname == NULL)
+{
+free(ndog);
+return (NULL);
+}
+/*_strcp(nname, name);*/
+
+nowner = malloc(sizeof(char) * _strl(owner) + 1);
+if (nowner == NULL)
+{
+free(ndog);
+free(nname);
+return (NULL);
+}
+_strcp(nname, name);
+_strcp(nowner, owner);
+
+(*ndog).name = nname;
+(*ndog).age = age;
+(*ndog).owner = nowner;
+
+return (ndog);
+}
+
+/**
+ *_strl - long of string
+ *@s: string
+ *Return: long of string
+ */
+int _strl(char *s)
+{
+int con;
+for (con = 0; s[con] != '\0'; con++)
+		;
+return (con);
+}
+
+/**
+ *_strcp - copy a string
+ *@dest: destiny
+ *@src: origin
+ *Return: copy of string
+ */
+char *_strcp(char *dest, char *src)
+{
+int con1, con2;
+
+for (con1 = 0; src[con1] != '\0'; con1++)
+;
+
+for (con2 = 0; con2 < con1; con2++)
+dest[con2] = src[con2];
+return (dest);
 }
