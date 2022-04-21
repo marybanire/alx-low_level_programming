690bd7093a6b86c80bb44a39d5d038a12cc04768
marybanire <baniremary@gmail.com>
2022-04-21 22:09:49 +0100

    print line before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * tortoiseAndHare - print line before main
+ * Return: nothing
+ **/
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
