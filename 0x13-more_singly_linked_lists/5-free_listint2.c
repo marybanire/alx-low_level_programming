044a8e3404d5ed62153b08a87bd47f65746d63e5
marybanire <baniremary@gmail.com>
2022-04-26 14:49:20 +0100

    free a listint_t list, sets head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,21 +6,19 @@
 /**
  * free_listint2 - free a listint_t list, sets head to NULL
  * @head: address of head of list
+ *
  * Return: nothing
  **/
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
-	listint_t *curr;
+	listint_t *friend;
 
-	if (head != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
+		friend = (*head)->next;
+		free(*head);
+		(*head) = friend;
 	}
 }
